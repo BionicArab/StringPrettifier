cbca67bacd22bd4d7e0f0f4ca05b96503081d354
Mohd <mohammed.abualhuda@outlook.com>
2020-06-07 11:12:29 +0200

    Initial Commit

---
+++ StringPrettifier/StringPrettifier.h
@@ -0,0 +1,19 @@
+//
+//  StringPrettifier.h
+//  StringPrettifier
+//
+//  Created by BionicArab G on 07.06.20.
+//  Copyright © 2020 BionicArab G. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringPrettifier.
+FOUNDATION_EXPORT double StringPrettifierVersionNumber;
+
+//! Project version string for StringPrettifier.
+FOUNDATION_EXPORT const unsigned char StringPrettifierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringPrettifier/PublicHeader.h>
+
+
